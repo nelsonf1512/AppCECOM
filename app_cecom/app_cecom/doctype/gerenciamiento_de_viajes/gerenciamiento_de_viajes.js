2853209ebfa91b5192bf1e25ea9546a8b6ee1a9d
nelsonf1512 <nelson.fernandez@mbcorp.mx>
2026-04-15 23:15:12 +0000

    Se sube primeros cambios para GV

---
+++ app_cecom/app_cecom/doctype/gerenciamiento_de_viajes/gerenciamiento_de_viajes.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Nelson Andres Fernandez and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Gerenciamiento de viajes", {
+// 	refresh(frm) {
+
+// 	},
+// });
