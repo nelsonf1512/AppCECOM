ef4abfec9917562c5337c130a240fb6b3836a33c
nelsonf1512 <nelson.fernandez@mbcorp.mx>
2026-09-09 19:08:09 +0000

    Se sube rol para control de ubicaciones

--- app_cecom/hooks.py
+++ app_cecom/hooks.py
@@ -255,7 +255,7 @@ fixtures = [
     {"doctype": "Role", "filters": [["name", "in", ["usuario_elabora_gerenciamiento_viajes","usuario_autoriza_gerenciamiento_viajes",
     "usuario_transito_gerenciamiento_viajes", "usuario_geocerca_gerenciamiento_viajes", "usuario_finalizar_gerenciamiento_viajes", "usuario_sustentabilidad",
     "usuario_autorizadores_gv","usuario_puntos_gv", "usuario_motivos_gv", "usuario_autoriza_qhse_gerenciamiento_viajes", "usuario_autoriza_cecom_gerenciamiento_viajes",
-    "usuario_autoriza_direccion_gerenciamiento_viajes", "usuario_cecom_finalizar_gerenciamiento_viajes"]]]},  
+    "usuario_autoriza_direccion_gerenciamiento_viajes", "usuario_cecom_finalizar_gerenciamiento_viajes", "usuario_control_ubicaciones"]]]},  
     {
         "doctype": "Custom DocPerm",
         "filters": [[
@@ -274,7 +274,8 @@ fixtures = [
                 "usuario_autoriza_qhse_gerenciamiento_viajes",
                 "usuario_autoriza_cecom_gerenciamiento_viajes",
                 "usuario_autoriza_direccion_gerenciamiento_viajes",
-                "usuario_cecom_finalizar_gerenciamiento_viajes"
+                "usuario_cecom_finalizar_gerenciamiento_viajes",
+                "usuario_control_ubicaciones"
             ]
         ]]
     },
